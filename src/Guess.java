8ea518c666fe0ed217500a94e50cafdb8219e223
EvilGray <hui892875925@163.com>
2014-06-24 17:06:27 +0800

    Signed-off-by: EvilGray <hui892875925@163.com>

---
+++ src/Guess.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Guess{
+		public static void main(String[] arge){
+			Scanner in=new Scanner(System.in);
+			System.out.println("木偶先生的猜拳游戏");
+			System.out.println("请亮出你的小屁拳     1=剪刀手    2=锤子手   3=布子手");
+			int person=in.nextInt();
+			int computer=(int)(Math.random()*3)+1;
+			System.out.println(computer);
+			int mark=
+		}
+	
+}
